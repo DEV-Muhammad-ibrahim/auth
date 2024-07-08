af2f1b67b5a625dd37be42463c2ba647bb7b4607
DEV-Muhammad-ibrahim <ibrahim97416@gmail.com>
2024-07-08 16:11:39 +0500

    db

--- server/connection.ts
+++ server/connection.ts
@@ -1,24 +1,22 @@
 import mongoose from "mongoose";
 
-
-export default async function connection() {
+interface ConnectionObject{
+  isConnected?:number
+}
+const connection:ConnectionObject = {}
+export default async function dbConnection() {
+  if(connection.isConnected){
+    console.log("Already Connected to database",connection.isConnected)
+    return
+  }
   try {
-    const connect = mongoose.connect(process.env.MONGO_DB_URI!);
-    const connection = mongoose.connection
-    connection.on("connected",() =>{
-      console.log("MONGO DB CONNECTED")
-    })
-    connection.on('error',(err) => {
-      console.log("Please connect to database" + err)
-      process.exit()
-    })
-    if(!connection){
-      console.log("Something is creepy")
-    }
-  
+    const db = await mongoose.connect(process.env.MONGO_DB_URI || '',{})
+    connection.isConnected = db.connections[0].readyState
+    console.log('db',db)
+    console.log('Connected to database')
   } catch (error:any) {
-    console.log("Something went wrong while connecting to database");
-    console.log(error)
+    console.log("Something went wrong while connecting to database",error);
+    process.exit(1)
   }
   
 }
